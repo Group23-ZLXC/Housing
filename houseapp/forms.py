18c106f7007cec1741c24db2ac1728f936f80b05
Cui <735068460@qq.com>
2021-03-17 20:06:00 +0800

    Log in & Sign up

--- houseapp/forms.py
+++ houseapp/forms.py
@@ -5,3 +5,24 @@ from wtforms.validators import DataRequired
 class CommentForm(FlaskForm):
     comment = TextAreaField('Your Comment', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+# render_kw: https://www.cnblogs.com/FRESHMANS/p/8529992.html
+class SignupForm(FlaskForm):
+    username = StringField('', validators=[DataRequired()], description="Username",
+                           render_kw={"class": "form-control", "placeholder": "Username", "required": 'required'})
+    email = StringField('', validators=[DataRequired()],
+                        render_kw={"class": "form-control", "placeholder": "Email", "required": 'required'})
+    password = PasswordField('', validators=[DataRequired()],
+                             render_kw={"class": "form-control", "placeholder": "Password", "required": 'required'})
+    password2 = PasswordField('', validators=[DataRequired()],
+                              render_kw={"class": "form-control", "placeholder": "Repeat Password",
+                                         "required": 'required'})
+
+    submit = SubmitField('Sign up', render_kw={"class": "btn btn-primary btn-block btn-flat"})
--- houseapp/routes.py
+++ houseapp/routes.py
@@ -1,6 +1,8 @@
-from houseapp import app
-from flask import render_template,url_for
-from houseapp.forms import CommentForm
+from houseapp import app, db
+from flask import render_template, flash, redirect, url_for, session, request, jsonify
+from houseapp.forms import CommentForm, LoginForm, SignupForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from houseapp.models import User
 
 @app.route('/')
 @app.route('/details')
@@ -11,3 +13,65 @@ def details():
 @app.route('/buy')
 def buy():
     return render_template('buy.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user_in_db = User.query.filter(User.username == form.username.data).first()
+        if not user_in_db:
+            flash('No user found with username: {}'.format(form.username.data))
+            return redirect(url_for('login'))
+        if check_password_hash(user_in_db.password_hash, form.password.data):
+            flash('Login success!')
+            session["USERNAME"] = user_in_db.username
+            return redirect(url_for('login'))
+        # should go to the logged-in page
+        flash('Incorrect Password')
+        return redirect(url_for('login'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password2.data:
+            flash('Passwords do not match!')
+            return redirect(url_for('signup'))
+        user_in_db = User.query.filter(User.username == form.username.data).first()
+        if user_in_db:
+            flash('Username already existed!')
+            return redirect(url_for('signup'))
+        passw_hash = generate_password_hash(form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password_hash=passw_hash)
+        db.session.add(user)
+        db.session.commit()
+        flash('User registered with username:{}'.format(form.username.data))
+        session["USERNAME"] = user.username
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Register a new user', form=form)
+
+
+@app.route('/checkuser', methods=['POST'])
+def check_username():
+    chosen_name = request.form['username']
+    user_in_db = User.query.filter(User.username == chosen_name).first()
+    if not user_in_db:
+        return jsonify({'text': 'Username is available',
+                        'returnvalue': 0})
+    else:
+        return jsonify({'text': 'Sorry! Username is already taken',
+                        'returvalue': 1})
+
+
+@app.route('/checkemail', methods=['POST'])
+def check_email():
+    chosen_email = request.form['email']
+    user_in_db = User.query.filter(User.email == chosen_email).first()
+    if not user_in_db:
+        return jsonify({'text': 'Email is available',
+                        'returnvalue': 0})
+    else:
+        return jsonify({'text': 'Sorry! Email is already taken',
+                        'returvalue': 1})
