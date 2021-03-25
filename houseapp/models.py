fd4e0c5fb0ca47422aa01663edb2ed70224f6546
ShivaniZhang29 <yu.zhang2@ucdconnect.ie>
2021-03-25 13:47:24 +0800

    update models.py

--- houseapp/models.py
+++ houseapp/models.py
@@ -1,10 +1,36 @@
 from houseapp import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    comment = db.relationship('Comment', backref='author', lazy='dynamic')
+    answer = db.relationship('Answer', backref='author', lazy='dynamic')
+    house = db.relationship('House', backref='owner', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class House(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    answers = db.relationship('Answer', backref='question', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Question asked at {} by {}: {} >'.format(str(self.timestamp)[0:10],self.author.username,self.body)
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
+    question_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
