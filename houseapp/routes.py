057f0403ac4147c5309304a54dd0020cd41e29fb
ShivaniZhang29 <yu.zhang2@ucdconnect.ie>
2021-04-26 21:08:08 +0800

    server

--- houseapp/routes.py
+++ houseapp/routes.py
@@ -19,9 +19,7 @@ def make_shell_context():
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
-if __name__ == '__main__':
-    manager.run()
-    
+
 @app.route('/')
 @app.route('/homepage', methods=['GET'])
 def homepage():
